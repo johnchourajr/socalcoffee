dbac0267400907c91a59aa0ce297ecb5f779815b
johnchourajr <hello@objectsubject.com>
2016-09-12 07:17:52 -0700

    added mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,21 +18,38 @@ $('a[href*=#]:not([href=#])').click(function() {
 
 $('.search-nav').on('click', function(evt) {
 
-    var body = $('body');
-    var button = $('.search-nav');
-    var field = $('.search-nav-field');
+  var body = $('body');
+  var button = $('.search-nav');
+  var drawer = $('.search-nav-drawer');
+  var field = $('.search-box');
+  var icn = $('.search-nav-icn');
 
-    // if (button.hasClass('closed')) {
-    //   button.addClass('open').removeClass('closed');
-    // } else {
-    //   button.addClass('closed').removeClass('open');
-    // }
+  if (drawer.hasClass('closed')) {
+    drawer.addClass('open').removeClass('closed');
+    field.focus();
+    icn.attr('src','/assets/images/x.svg');
 
-    if (field.hasClass('closed')) {
-      field.addClass('open').removeClass('closed');
-      body.addClass('noscroll');
-    } else {
-      field.addClass('closed').removeClass('open');
-      body.removeClass('noscroll');
-    }
-  });
+  } else {
+    drawer.addClass('closed').removeClass('open');
+    icn.attr('src','/assets/images/search.svg');
+
+  }
+});
+
+$('.menu-nav').on('click', function(evt) {
+
+  var body = $('body');
+  var button = $('.menu-nav');
+  var drawer = $('.menu-nav-drawer');
+  var icn = $('.menu-nav-icn');
+
+  if (drawer.hasClass('closed')) {
+    drawer.addClass('open').removeClass('closed');
+    icn.attr('src','/assets/images/x.svg');
+
+  } else {
+    drawer.addClass('closed').removeClass('open');
+    icn.attr('src','/assets/images/menu.svg');
+
+  }
+});
